687a51507b5dc13a83a0292c12070af15d12c21d
Constantino Fernandez Traba <c.fernandez.traba@mobgen.com>
2018-12-23 15:01:32 +0100

    first immer working state

--- src/containers/ContactList/index.js
+++ src/containers/ContactList/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import compose from "recompose/compose";
-import withHandlers from "recompose/withHandlers";
-import lifecycle from "recompose/lifecycle";
 import List from "material-ui/List";
 import ListItem from "material-ui/List/ListItem";
 import Avatar from "material-ui/Avatar";
@@ -15,78 +11,66 @@ import DeleteIcon from "material-ui/svg-icons/action/delete";
 import ContactCard from "../../components/ContactCard";
 import { LIST_MODE } from "../../models/contactList/constants";
 import { EDIT_PATHNAME, DETAIL_PATHNAME } from "../../globals/pathNames";
-import selectContactListDomain from "../../models/contactList/selectors";
-import { makeSelectContactListPopulated } from "../../models/contacts/selectors";
+import { Subscribe } from 'bey';
+import Contacts from '../../state/contacts/';
 
-function ListContactPage(props) {
-  return (
-    <div style={{ margin: "0.2em 0 0 0" }}>{props.renderContactList()}</div>
+function addHandlers(props){
+  return { 
+    ...props, 
+    transitionToEditContact: (id) =>
+      props.history.push(`${EDIT_PATHNAME}/${id}`),
+    transitionToContactDetail: (id) =>
+      props.history.push(`${DETAIL_PATHNAME}/${id}`),
+  };
+}
+
+
+function ContactListCards(props){
+  return props.list.map(contact => (
+    <ContactCard
+      key={`contact-${contact.id}`}
+      contact={contact}
+      onEditClick={() => props.transitionToEditContact(contact.id)}
+      onDeleteClick={() => props.deleteContact(contact.id)}
+    />
+  ));
+}
+
+
+function ContactListItems(props){
+  
+  return props.list.map(contact => 
+    <div key={`contact-${contact.id}`}>
+      <ListItem
+        leftAvatar={
+          contact.imgUrl ? (
+            <Avatar src={contact.imgUrl} />
+          ) : (
+            <Avatar>{contact.name.substring(0, 1)}</Avatar>
+          )
+        }
+        primaryText={contact.name}
+        secondaryText={contact.groups || "Without group"}
+        secondaryTextLines={1}
+        onClick={() => props.transitionToContactDetail(contact.id)}
+        rightIconButton={
+          <IconButton>
+            <DeleteIcon onClick={() => props.deleteContact(contact.id)} />
+          </IconButton>
+        }
+      />
+      <Divider key={`divider-${contact.id}`} inset />
+    </div>
   );
 }
 
-ListContactPage.propTypes = {
-  renderContactList: PropTypes.func.isRequired
-};
-
-const mapStateToProps = createStructuredSelector({
-  contacts: makeSelectContactListPopulated(),
-  settings: selectContactListDomain()
-});
-
-const mapDispatchToProps = dispatch => ({
-  initializeView: () => dispatch.contacts.requestContactList(),
-  deleteContact: id => dispatch.contacts.deleteContactRequest(id)
-});
-
-export default compose(
-  connect(mapStateToProps, mapDispatchToProps),
-  withHandlers({
-    transitionToEditContact: props => id =>
-      props.history.push(`${EDIT_PATHNAME}/${id}`),
-    transitionToContactDetail: props => id =>
-      props.history.push(`${DETAIL_PATHNAME}/${id}`)
-  }),
-  withHandlers({
-    renderContactListItems: props => () =>
-      props.contacts.map(contact => (
-        <div key={`contact-${contact.id}`}>
-          <ListItem
-            leftAvatar={
-              contact.imgUrl ? (
-                <Avatar src={contact.imgUrl} />
-              ) : (
-                <Avatar>{contact.name.substring(0, 1)}</Avatar>
-              )
-            }
-            primaryText={contact.name}
-            secondaryText={contact.groups || "Without group"}
-            secondaryTextLines={1}
-            onClick={() => props.transitionToContactDetail(contact.id)}
-            rightIconButton={
-              <IconButton>
-                <DeleteIcon onClick={() => props.deleteContact(contact.id)} />
-              </IconButton>
-            }
-          />
-          <Divider key={`divider-${contact.id}`} inset />
-        </div>
-      )),
-    renderContactCardList: props => () =>
-      props.contacts.map(contact => (
-        <ContactCard
-          key={`contact-${contact.id}`}
-          contact={contact}
-          onEditClick={() => props.transitionToEditContact(contact.id)}
-          onDeleteClick={() => props.deleteContact(contact.id)}
-        />
-      ))
-  }),
-  withHandlers({
-    renderContactList: props => () => {
-
-      if (props.settings.mode === LIST_MODE) {
-        return (
-          <List>
+export default function ContactList(props){
+  
+  return (
+    <div style={{ margin: "0.2em 0 0 0" }}>
+      <Subscribe to={Contacts.state}>
+        {contacts => {return (
+          (true)?<List>
             <div style={{
               boxSizing: "border-box",
               color: "rgba(0, 0, 0, 0.54)",
@@ -98,28 +82,23 @@ export default compose(
             }}>
               Contacts
             </div>
-            {props.renderContactListItems()}
+            <ContactListItems {...addHandlers(props)} list={contacts.list} />
           </List>
-        );
-      }
-      return (
-        <div
-          style={{
-            display: "flex",
-            flex: "0 0 auto",
-            flexWrap: "wrap",
-            margin: "1em",
-            justifyContent: "space-between"
-          }}
-        >
-          {props.renderContactCardList()}
-        </div>
-      );
-    }
-  }),
-  lifecycle({
-    componentDidMount() {
-      this.props.initializeView();
-    }
-  })
-)(ListContactPage);
+          :    
+          <div style={{
+              display: "flex",
+              flex: "0 0 auto",
+              flexWrap: "wrap",
+              margin: "1em",
+              justifyContent: "space-between"
+            }}
+          >
+            <ContactListCards {...addHandlers(props)} list={contacts.list} />            
+          </div>
+        )}}          
+      </Subscribe>
+    </div>
+  );  
+}
+
+Contacts.actions.loadData();
