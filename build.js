f2f14b1c3b4636974530087e29a289515db7711b
Constantino Fernandez Traba <c.fernandez.traba@mobgen.com>
2018-12-26 03:59:00 +0100

    optimized icons

---
+++ build.js
@@ -0,0 +1,25 @@
+process.env.NODE_ENV = "production"
+var BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin
+
+const path = require('path');
+const { NormalModuleReplacementPlugin } = require('webpack');  
+
+const webpackConfigProd = require("react-scripts/config/webpack.config.prod")
+/*
+webpackConfigProd.plugins.push(
+  new BundleAnalyzerPlugin({
+    analyzerMode: "static",
+    reportFilename: "report.html",
+  })
+)*/
+
+webpackConfigProd.plugins.push(
+  new NormalModuleReplacementPlugin(
+    /.*\/generated\/iconSvgPaths.*/,
+    path.resolve(__dirname, './iconSvgPaths.js'),
+  )
+)
+
+require("react-scripts/scripts/build")
+
