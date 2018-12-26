10476c554bf4ad9063334415d301a2fa453c37ed
Constantino Fernandez Traba <c.fernandez.traba@mobgen.com>
2018-12-26 17:00:59 +0100

    removed comments

--- src/state/helpers/loading.js
+++ src/state/helpers/loading.js
@@ -6,19 +6,17 @@ export default function loading(stateModule){
 	if(stateModule.actions && Object.values(stateModule.actions).length > 0){
   	Object.keys(stateModule.actions).forEach(actionName => {
       if(stateModule.actions[actionName] && typeof stateModule.actions[actionName] === 'function' ){
-        //const oldName = action.name;
+
         const originalAction = stateModule.actions[actionName];
+
         stateModule.actions[actionName] = async function(...args){
-          //console.log('loading true');          
           GlobalState.actions.setLoading(true, stateModule.name);          
           update(stateModule.state, state => { state.loading = true; });
           const result = await originalAction(...args);
           update(stateModule.state, state => { state.loading = false; });
           GlobalState.actions.setLoading(false, stateModule.name);
-          //console.log('loading false');
           return result;
-        }
-        //action.name = oldName + "Loading";     
+        }   
       }  		
   	});
 	}
