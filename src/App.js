656dab717f505d29c464a2a62caa88a966c18590
Constantino Fernandez Traba <c.fernandez.traba@mobgen.com>
2019-01-01 13:22:22 +0100

    from component to function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Route } from 'react-router5';
 import PropTypes from 'prop-types';
 import ComponentSelector from './router/ComponentSelector';
@@ -7,25 +7,25 @@ import Loader from './containers/Loader';
 import Snackbar from './containers/Snackbar';
 import LeftMenu from './containers/Menu/LeftMenu';
 
-class App extends PureComponent {
-  render() {
-    const { router } = this.props;
-    return (
-      <div className="bp3-fill" style={{ backgroundColor: '#30404d', minHeight: '100vh' }}>
-        <Route router={router}>{({ route }) => <Navbar route={route} />}</Route>
-        <Loader />
-        <Route router={router}>
-          {({ route }) => (
-            <div style={{ display: 'flex' }}>
-              <LeftMenu route={route} />
-              <ComponentSelector route={route} {...this.props} />
-            </div>
-          )}
-        </Route>
-        <Snackbar />
-      </div>
-    );
-  }
+function App({
+  router,
+  ...rest
+}) {
+  return (
+    <div className="bp3-fill" style={{ backgroundColor: '#30404d', minHeight: '100vh' }}>
+      <Route router={router}>{({ route }) => <Navbar route={route} />}</Route>
+      <Loader />
+      <Route router={router}>
+        {({ route }) => (
+          <div style={{ display: 'flex' }}>
+            <LeftMenu route={route} />
+            <ComponentSelector route={route} {...rest} />
+          </div>
+        )}
+      </Route>
+      <Snackbar />
+    </div>
+  );
 }
 
 App.propTypes = {
